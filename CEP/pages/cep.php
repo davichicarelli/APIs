23608dc03e9ef7dc370fe129643ccdfc5b922943
DAVI CHICARELLI PEREIRA <davi.cpereira4@senacsp.edu.br>
2025-09-23 17:05:20 -0300

    commit inicial. APIs

--- CEP/pages/cep.php
+++ CEP/pages/cep.php
@@ -25,43 +25,60 @@ if (isset($cep) == false || strlen($cep) != 8) {
     // json_decode() é uma função do PHP que transforma uma string no formato JSON em um array associativo.
 
     if (isset($dados['erro']) == true) {
-      $mensagem = "<h2>CEP não encontrado!</h2>";
-    } else {
-      $mensagem = "
-    <h1>CEP encontrado!</h1><br>
-    <p>Endereço:</p>
-    <input type='text' value='{$dados['logradouro']}' disabled><br>
-    <p>Número e Complemento:</p>
-    <input type='text' value='{$dados['complemento']}'><br>
-    <p>Bairro:</p>
-    <input type='text' value='{$dados['bairro']}' disabled><br>
-    <p>Cidade:</p>
-    <input type='text' value='{$dados['localidade']}' disabled><br>
-    <p>Estado:</p>
-    <input type='text' value='{$dados['estado']}' disabled><br>
-    ";
-      //echo "<h2>Endereço encontrado!</h2>";
-      //echo "<input type='text' value='${dados['logradouro']}'><br>";
-      //echo "<input type='text' value='${dados['complemento']}'><br>";
-      //echo "<input type='text' value='${dados['bairro']}'><br>";
-      //echo "<input type='text' value='${dados['localidade']}'><br>";
-      //echo "<input type='text' value='${dados['estado']}'><br>";
+      $mensagem = "<h2>CEP não encontrado!</h2><br>";
+     } else {
+       $mensagem = "<h1>CEP encontrado!</h1><br>";
+    // <h1>CEP encontrado!</h1><br>
+    // <p>Endereço:</p>
+    // <input type='text' value='{$dados['logradouro']}' disabled><br>
+    // <p>Número e Complemento:</p>
+    // <input type='text' value='{$dados['complemento']}'><br>
+    // <p>Bairro:</p>
+    // <input type='text' value='{$dados['bairro']}' disabled><br>
+    // <p>Cidade:</p>
+    // <input type='text' value='{$dados['localidade']}' disabled><br>
+    // <p>Estado:</p>
+    // <input type='text' value='{$dados['estado']}' disabled><br>
+    // ";
     }
   }
 }
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
-
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./../css/style.css">
   <title>Consulta de CEP</title>
 </head>
-
 <body>
-  <?= $mensagem ?>
+  <div id="cep-buscado">
+    <span id="error"> <?= $mensagem ?></span>
+  <div>
+     <p>Endereço:</p>
+     <input type="text" value="<?= isset($dados['logradouro']) ? $dados ['logradouro'] : ''?>" disabled>
+     </div>
+
+     <div><br>
+     <p>Número e Complemento:</p>
+     <input type="text" value="<?= isset($dados['complemento']) ? $dados ['complemento'] : ''?>" disabled>
+     </div>
+
+     <div><br>
+     <p>Bairro:</p>
+     <input type="text" value="<?= isset($dados['bairro']) ? $dados ['bairro'] : ''?>" disabled>
+     </div>
+
+     <div><br>
+    <p>Cidade:</p>
+    <input type="text" value="<?= isset($dados['localidade']) ? $dados ['localidade'] : ''?>" disabled>
+    </div>
+
+    <div><br>
+    <p>Estado:</p>
+    <input type="text" value="<?= isset($dados['estado']) ? $dados ['estado'] : ''?>" disabled>
+    </div>
+  </div>
 </body>
 </html>
